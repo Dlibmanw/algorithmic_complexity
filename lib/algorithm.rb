1eaa4385942f851eca6064519828c653069f8fce
Dafna Libman <dafnalibman@gmail.com>
2020-05-18 12:28:41 +0100

    Update method to create 4 arrays, run the reverse method on each array, and return the execution time + array size

--- lib/algorithm.rb
+++ lib/algorithm.rb
@@ -1,13 +1,20 @@
 def execution_time
-
+  array = []
   start_time = Time.now
   arr1 = (0..5000).step(5000).to_a
+  array.push(arr1)
   arr2 = (0..10000).step(5000).to_a
+  array.push(arr2)
   arr3 = (0..50000).step(5000).to_a
+  array.push(arr3)
   arr4 = (0..100000).step(5000).to_a
-  finish_time = Time.now
-  exec_time = finish_time - start_time 
-  #p n.last(1)
-  p arr1.last(1), arr2.last(1), arr3.last(1), arr4.last(1)
-  p exec_time
+  array.push(arr4)
+  array.each do |arr| 
+    start_time = Time.now
+    arr.reverse
+    finish_time = Time.now
+    exec_time = finish_time - start_time 
+    p exec_time
+    p arr.length
+  end
 end
