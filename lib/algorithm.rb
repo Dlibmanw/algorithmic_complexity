3d69808f5a38836a4d8468637796fed6c79a4200
Elena <elena.beccaro@gmail.com>
2020-05-18 12:10:40 +0100

    create diffeternts arrays

--- lib/algorithm.rb
+++ lib/algorithm.rb
@@ -1,8 +1,13 @@
 def execution_time
-  names = ["Elena", "Dafna"]
+
   start_time = Time.now
-  names.last(1)
+  arr1 = (0..5000).step(5000).to_a
+  arr2 = (0..10000).step(5000).to_a
+  arr3 = (0..50000).step(5000).to_a
+  arr4 = (0..100000).step(5000).to_a
   finish_time = Time.now
   exec_time = finish_time - start_time 
+  #p n.last(1)
+  p arr1.last(1), arr2.last(1), arr3.last(1), arr4.last(1)
   p exec_time
 end
